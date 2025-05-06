f98778b7909603437381f82af4d057157259d041
Oliver Oayda <oaydaoliver@gmail.com>
2025-05-06 20:30:58 +1000

    add MapLoader class for reading .fits maps

--- utils/map_read.py
+++ utils/map_read.py
@@ -1,8 +1,44 @@
-class MapLoader:
-    def __init__(self):
-        pass
+from typing import Literal
+import healpy as hp
+from numpy.typing import NDArray
+import numpy as np
 
-    def load(
-            map_type: str
-    ) -> None:
-        pass
+class MapLoader:
+    def __init__(self,
+            briggs_weighting: Literal[-1, 0, 1],
+            configuration: Literal['AA', 'AA4']
+    ):
+        '''
+        Class for loading in SKA maps. Call the load method to return a map.
+
+        :param briggs_weighting: Briggs weighting used to generate the map.
+        :param configuration: SKA telescope configuration used to generate the map.
+        '''
+        if briggs_weighting == -1:
+            self.briggs_weighting == 'n1'
+        else:
+            self.briggs_weighting = str(briggs_weighting)
+        
+        self.configuration = configuration
+
+    def load(self,
+            map_number: int
+    ) -> NDArray[np.int_]:
+        '''
+        Load and return SKA fits density map based on congigured settings
+        and specified map number.
+
+        :param map_number: Number appearing in fits file, e.g. `map_1.fits`
+            refers to map 1.
+        :return: Healpy density map.
+        '''
+        self.map_number = map_number
+        self.file_path = (
+            f'data/ska/briggs_{self.briggs_weighting}/{self.configuration}'
+        )
+        self.file_name = f'map_{self.map_number}.fits'
+        self.density_map = hp.read_map(
+            f'{self.file_path}/{self.file_name}',
+            nest=False
+        )
+        return self.density_map
