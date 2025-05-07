5d614c5a6c7ee4635ef22b8d9858cce56e69914e
VasudevMittal <vmit0571@uni.sydney.edu.au>
2025-05-07 18:54:33 +1000

    tested the code and removed some bugs

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,5 +1,6 @@
 from numpy.typing import NDArray
-from matplotlib import Axes, Figure
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 import numpy as np
 import healpy as hp
 from utils.physics import omega_to_theta
@@ -10,113 +11,91 @@ class MapPlotter:
     def __init__(self, density_map: NDArray[np.int_]) -> None:
         self.density_map = density_map
         self.default_settings = {
-            'graticule': True,
-            'graticule_labels': True,
+            'cbar': True,
+            'cb_orientation': 'vertical',
             'phi_convention': 'counterclockwise',
-            'graticule_color': 'black',
-            'longitude_grid_spacing': 90, 
-            'latitude_grid_spacing': 45,
             'badcol':'white',
             'fontsize':{'title':'18','xtick_label':'15','ytick_label':'15'},
-            'custom_xtick_labels': ["$90^\circ$","$0^\circ$","$270^\circ$",],
-            'custom_ytick_labels': ["$-45^\circ$","$0^\circ$","$45^\circ$"]
         }
 
-    def plot_density_map(self, #self, **projview_kwargs) -> None:
-                        cmap:str,
-                        cbar_orientation: str,
-                        frames: str,
-                        projview_kwargs: dict = {},
+    def plot_density_map(self,
+                        cmap: str = 'viridis',
                         cmap_alpha: float=1.0,
+                        projview_kwargs: dict = {},
         ) ->  tuple[Axes, Figure]:
         '''
         Plot map smoothed with a moving average.
-        :param cbar_orientation: Orientation of the colorbar.
-            Can be 'horizontal' or 'vertical'.
-        :param frames: Coordinate system to use for the plot. 
-            Can be 'C' for Equatorial, 'G' for Galactic, 
-            'CG' for transforming frame from Equatorial to Galactic, 
-            or 'GC' for transforming frame from Galactic to Equatorial.
-        :param projview_kwargs: Keyword arguments to pass to `hp.projview`.
+        
+        :param cmap: Colormap to use for the plot.
+            Default is 'viridis'.
         :param cmap_alpha: Transparency of the colormap.
             0.0 is fully transparent, 1.0 is fully opaque.
+        :param projview_kwargs: Keyword arguments to pass to `hp.projview`.
+        
+        :return: Tuple of the axes and figure objects.
         '''
-        # hp.projview(
-        #     self.density_map,
-        #     {
-        #         **self.default_settings,
-        #         **projview_kwargs
-        #     }
-        # )
-        if 'title' in projview_kwargs:
-            map_title = projview_kwargs['title']
-        else:
-            map_title = 'Density map'
-        hp.projview(map,
-                    {**self.default_settings, 
-                     **projview_kwargs},
-                    title=map_title,
-                    cmap=self.cmap_scaled(cmap, alpha=cmap_alpha),
-                    cb_orientation=cbar_orientation,
-                    coord=frames,
+        projview_dict = {}
+        for key in self.default_settings:
+            if key not in projview_kwargs:
+                projview_dict[key] = self.default_settings[key]
+        for key in projview_kwargs:
+                projview_dict[key] = projview_kwargs[key]
+        
+        if 'title' not in projview_dict:
+            projview_dict['title'] = 'Density map'
+            
+        hp.projview(self.density_map,
+                    cmap=MapPlotter.cmap_scaled(cmap, cmap_alpha),
+                    **projview_dict,
                     override_plot_properties={'vertical_tick_rotation':True 
-                                              if cbar_orientation=='vertical' 
-                                              else False,'cbar_shrink':1 
-                                              if cbar_orientation=='vertical' 
+                                              if projview_dict.get('cb_orientation')=='vertical' 
+                                              else False,'cbar_shrink':0.5
+                                              if projview_dict.get('cb_orientation')=='vertical' 
                                               else 0.5},
                     )
         return plt.gca(),plt.gcf()
     
-    
     def plot_smooth_map(self,
-                        cmap:str,
-                        cbar_orientation: str,
-                        frames: str,
+                        cmap: str = 'viridis',
+                        cmap_alpha: float=1.0,
                         moving_average_kwargs: dict = {},
                         projview_kwargs: dict = {},
-                        cmap_alpha: float = 1.0,
         ) ->  tuple[Axes, Figure]:
         '''
         Plot map smoothed with a moving average.
-        :param cbar_orientation: Orientation of the colorbar.
-            Can be 'horizontal' or 'vertical'.
-        :param frames: Coordinate system to use for the plot. 
-            Can be 'C' for Equatorial, 'G' for Galactic, 
-            'CG' for transforming frame from Equatorial to Galactic, 
-            or 'GC' for transforming frame from Galactic to Equatorial.
+        
+        :param cmap: Colormap to use for the plot.
+            Default is 'viridis'.
+        :param cmap_alpha: Transparency of the colormap.
+            0.0 is fully transparent, 1.0 is fully opaque.
         :param moving_average_kwargs: Keyword arguments to pass to
             `MapPlotter.moving_average_smooth`.
         :param projview_kwargs: Keyword arguments to pass to `hp.projview`.
-        :param cmap_alpha: Transparency of the colormap.
-            0.0 is fully transparent, 1.0 is fully opaque.
+        
+        :return: Tuple of the axes and figure objects.
         '''
         self.smoothed_map = self.moving_average_smooth(
             self.density_map,
             **moving_average_kwargs
         )
-        # hp.projview(
-        #     self.smoothed_map,
-        #     {
-        #         **self.default_settings,
-        #         **projview_kwargs
-        #     }
-        # )
-        if 'title' in projview_kwargs:
-            map_title = projview_kwargs['title']
-        else:
-            map_title = 'Density map smoothed with moving average over'
-            +str(moving_average_kwargs.get('angle_scale'))+' steradians'
-        hp.projview(map,
-                    {**self.default_settings, 
-                     **projview_kwargs},
-                    title=map_title,
-                    cmap=self.cmap_scaled(cmap, alpha=cmap_alpha),
-                    cb_orientation=cbar_orientation,
-                    coord=frames,
+        
+        projview_dict = {}
+        for key in self.default_settings:
+            if key not in projview_kwargs:
+                projview_dict[key] = self.default_settings[key]
+        for key in projview_kwargs:
+                projview_dict[key] = projview_kwargs[key]
+        
+        if 'title' not in projview_dict:
+            projview_dict['title'] = 'Density map smoothed with moving average over'+str(moving_average_kwargs.get('angle_scale'))+' steradians'
+            
+        hp.projview(self.smoothed_map,
+                    cmap=MapPlotter.cmap_scaled(cmap, cmap_alpha),
+                    **projview_dict,
                     override_plot_properties={'vertical_tick_rotation':True 
-                                              if cbar_orientation=='vertical' 
-                                              else False,'cbar_shrink':1 
-                                              if cbar_orientation=='vertical' 
+                                              if projview_dict.get('cb_orientation')=='vertical' 
+                                              else False,'cbar_shrink':0.5
+                                              if projview_dict.get('cb_orientation')=='vertical' 
                                               else 0.5},
                     )
         return plt.gca(),plt.gcf()
@@ -138,9 +117,11 @@ class MapPlotter:
             average, for example if one wants to weight by a selection function.
         :param angle_scale: Angle in steradians over which to apply the moving
             average.
+            
+        :return: Smoothed healpy map.
         '''
         included_pixels = np.where(~np.isnan(density_map))[0]
-        smoothed_map = np.empty_like(density_map)
+        smoothed_map = np.empty_like(density_map,dtype=np.float64)
         smoothed_map.fill(np.nan)
         nside = hp.get_nside(density_map)
         smoothing_radius = omega_to_theta(angle_scale)
@@ -158,18 +139,24 @@ class MapPlotter:
 
         return smoothed_map
 
+    @staticmethod
     def cmap_scaled(
                     cmap: str,
                     alpha: float,
                     ) -> ListedColormap:
         '''
         Enhance the transparency of a colormap.
+        
         :param cmap: Colormap to enhance.
         :param alpha: Transparency of the colormap.
             0.0 is fully transparent, 1.0 is fully opaque.
+            
+        :return: Enhanced colormap.
         '''
+        cmap = plt.get_cmap(cmap)
         my_cmap = cmap(np.arange(cmap.N))
         my_cmap[:,0:3] *= alpha
         my_cmap[:,0:3] += np.array([1,1,1])*(1-alpha)
+        
         my_cmap = ListedColormap(my_cmap)
         return my_cmap
