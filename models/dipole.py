7f90feb9fdc107affc72069143464a9ed1bbd5a5
Oliver Oayda <oaydaoliver@gmail.com>
2025-05-08 16:52:50 +1000

    add a Dipole model for performing Bayesian inference on density maps
    - Add ultranest_logs/ to the gitignore, which is where the results of the NS run are saved by default.
    - Add h5py and ipywidgets to the requirements, which are needed by ultranest when using interactive windows.

--- models/dipole.py
+++ models/dipole.py
@@ -1,12 +1,17 @@
 from numpy.typing import NDArray
 import numpy as np
 from scipy.stats import poisson
+from utils.inference import Inference
+from typing import Literal
+from models.priors import Prior
+import healpy as hp
+from utils.math import compute_dipole_signal
 
 class Likelihood:
     def __init__(self):
         pass
 
-    def point_by_point_log_likelihood(
+    def point_by_point_log_likelihood(self,
             dipole_signal: NDArray[np.float64],
             density_map: NDArray[np.int_]
     ) -> NDArray[np.float64]:
@@ -33,7 +38,7 @@ class Likelihood:
         )
         return log_likelihood
 
-    def poisson_log_likelihood(
+    def poisson_log_likelihood(self,
             rate_parameter: NDArray[np.float64],
             density_map: NDArray[np.int_]
     ) -> NDArray[np.float64]:
@@ -52,17 +57,158 @@ class Likelihood:
             mu=rate_parameter
         )
         return np.sum(log_likelihood_map, axis=0)
-
-class Dipole(Likelihood):
-    def __init__(self):
-        pass
-
+    
     def prior_transform(self,
             uniform_deviates: NDArray[np.float64]
         ) -> NDArray[np.float64]:
-        pass
+        '''
+        Meta function passed to the Nested Sampler's prior input; calls
+        `Prior` object's `transform` method to turn deviates on the unit cube
+        to deviates in prior space.
+        '''
+        return self.prior.transform(uniform_deviates)
 
-    def model(
+class Dipole(Likelihood, Inference):
+    def __init__(self,
+            density_map: NDArray[np.int_],
+            prior: Prior | None = None,
+            likelihood: Literal['point', 'poisson'] = 'point',
+    ):
+        '''
+        A pure dipole model. Depending on the choice of likelihood function,
+        this model has 3 or 4 parameters (see below). Note that this model is
+        vectorized by default in the sense that it can handle calls from the
+        Nested Sampler with many live points at once.
+
+        :param density_map:
+            Healpy source density map, of shape (n_pix,).
+        :param prior:
+            Pass either an instance of a Prior object or leave as None.
+            
+            If no class is specified, the prior uses default dipole priors.
+            These priors are specified in `models/default_priors.py`, with one
+            key exception: if the Poisson likelihood is specified, the
+            prior on the mean count parameter N will be automatically updated
+            to a uniform distribuion 25% either side of the mean of the density
+            map.
+            
+            In addition, if one specifies the point-by-point likelihood,
+            the mean count parameter N is removed from the prior distribution
+            and the dimensionality of the model is therefore reduced by 1.
+        :param likelihood:
+            Specify the type of likelihood function to use at inference:
+            
+            - `'poisson'` for the Poisson-based likelihood, or;
+            - `'point'` for the point-by-point likelihood.
+            
+            As mentioned above, this choice will dynamically change the model
+            dimensionality and the prior distributions.
+        '''
+        self.density_map = density_map
+        self._get_healpy_map_attributes()
+        self._parse_prior_choice(prior)
+        self._parse_likelihood_choice(likelihood)
+        self.parameter_names = self.prior.parameter_names
+        self.ndim = self.prior.ndim
+    
+    def _get_healpy_map_attributes(self) -> None:
+        '''
+        Retrieve key properties of a healpix map.
+        '''
+        self.mean_density = np.nanmean(self.density_map)
+        self.nside = hp.get_nside(self.density_map)
+        self.npix = hp.nside2npix(self.nside)
+        pixels_x, pixels_y, pixels_z = hp.pix2vec(
+            self.nside,
+            np.arange(self.npix)
+        )
+        # this will have shape (n_pixels, 3)
+        self.pixel_vectors = np.stack([pixels_x, pixels_y, pixels_z]).T
+
+    def _parse_prior_choice(self, prior: Prior | None = None) -> None:
+        '''
+        Switch to a default prior if the user has not specified one, or use
+        the explicit one the user has provided.
+        '''
+        if prior is None:
+            self.prior = Prior(choose_default='dipole')
+        else:
+            self.prior = prior
+
+    def _parse_likelihood_choice(self,
+            likelihood: Literal['point', 'poisson']
+        ) -> None:
+        '''
+        If one specifies the point-by-point likelihood, we don't need to fit
+        for the mean density. This function removes that parameter from the
+        list of priors and parameter names, reducing the dimension of the model
+        by 1.
+        
+        In addition, if one chooses the Poisson likelihood, we ideally
+        want the choice of mean density prior to center around the mean density
+        itself. This automatically makes that change without needing explicit
+        input from the user.
+        '''
+        self.likelihood = likelihood
+
+        if self.likelihood == 'point':
+            self.prior.remove_prior(prior_index=0)
+        elif self.likelihood == 'poisson':
+            self.prior.change_prior(
+                prior_index=0,
+                new_prior=[
+                    'Uniform',
+                    0.75 * self.mean_density,
+                    1.25 * self.mean_density
+                ]
+            )
+        else:
+            raise Exception(
+                f'Likelihood choice ({self.likelihood}) not recognised.'
+            )
+
+    def log_likelihood(self,
             Theta: NDArray[np.float64]
     ) -> NDArray[np.float64]:
-        pass
+        '''
+        Log likelihood function passed to the Nested Sampler. Automatically
+        adjusted depending on the user-specified prior.
+
+        :param Theta: Parameter samples from the prior distribtuion, as
+            generated by the Nested Sampler.
+        '''
+        dipole_term = self.model(Theta)
+        mean_density = Theta[:, 0]
+
+        if self.likelihood == 'point':
+            return self.point_by_point_log_likelihood(
+                dipole_signal=dipole_term,
+                density_map=self.density_map
+            )
+        else:
+            return self.poisson_log_likelihood(
+                rate_parameter=mean_density * dipole_term,
+                density_map=self.density_map
+            )
+
+    def model(self,
+            Theta: NDArray[np.float64]
+    ) -> NDArray[np.float64]:
+        '''
+        Evaluates 1 + D cos(theta) for the dipole model.
+        
+        :param Theta: Model prior samples, of shape (n_live, n_dim).
+        :return: Vectorised evaluation of 1 + D cos(theta), of shape
+            (n_pix, n_live).
+        '''
+        dipole_amplitude = Theta[:, -3]
+        dipole_longitude = Theta[:, -2]
+        dipole_colatitude = Theta[:, -1]
+
+        dipole_signal = compute_dipole_signal(
+            dipole_amplitude=dipole_amplitude,
+            dipole_longitude=dipole_longitude,
+            dipole_colatitude=dipole_colatitude,
+            pixel_vectors=self.pixel_vectors
+        )
+        return 1 + dipole_signal
