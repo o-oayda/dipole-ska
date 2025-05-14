edadf15e0b81c4333c5450ef687222a9f36b313f
Oliver Oayda <oaydaoliver@gmail.com>
2025-05-14 11:06:02 +1000

    Force a truncation of lmax=512 when using modulated_map method
    - Assert nside=512 for maps passed to ModulatedMapGenerator
    - Update TODO

--- TODO.md
+++ TODO.md
@@ -1,19 +1,19 @@
 # TODO
 
-- Add dipole to all maps and recover power spectrum
-    - **Geraint** will ask Sebastian
-- Go through each map and see how (in)consistent they are with the input map
+- [ ] Add dipole to all maps and recover power spectrum
+    - [x] **Geraint** will ask Sebastian about the ell truncation
+- [ ] Go through each map and see how (in)consistent they are with the input map
     - **Mali** to make pipeline (multiprocessing)
-- Implement sky plot
+- [ ] Implement sky plot
     - **Oliver**
-- Go through modulated_map function and comment
+- [ ] Go through modulated_map function and comment
     - **Oliver**
-- Summary statistics for outputs
+- [ ] Summary statistics for outputs
     - **Mali** to do
     - BF comparing CMB dipole vs fitted one
     - Model 0: D_0, phi_0, theta_0 (fixed)
     - Model 1: Free dipole fit
     - `model1.evidence - model0.evidence`
-- Can we test what happens if we set lmax=512 in both decomposition and reconstruction
+- [ ] Can we test what happens if we set lmax=512 in both decomposition and reconstruction
     - Is the small scale stuff being truncated causing the negative cells in the map?
     - **Vasudev** to do
--- dipoleska/utils/modulated_map_generator.py
+++ dipoleska/utils/modulated_map_generator.py
@@ -25,7 +25,7 @@ class ModulatedMapGenerator:
         self.dipole_longitude = dipole_longitude
         self.dipole_latitude = dipole_latitude
 
-    def dipole_map(self, nside: int) -> NDArray[np.complex_]:
+    def dipole_map(self, nside: int) -> NDArray[np.float64]:
         '''
         Calcuate the dipolar modulation map for an NSIDE, and the dipole parameters.
         
@@ -34,48 +34,49 @@ class ModulatedMapGenerator:
         :return: An all-sky map of dipolar modulation.
         '''
         pixel_vectors = hp.pix2vec(nside, np.arange(hp.nside2npix(nside)))
-        dipole = self.dipole_amplitude * hp.ang2vec(self.dipole_longitude,
-                                                    self.dipole_latitude,lonlat=True)
+        dipole = self.dipole_amplitude * hp.ang2vec(
+            self.dipole_longitude,
+            self.dipole_latitude,
+            lonlat=True
+        )
         stacked_vectors = np.vstack(pixel_vectors).T
-        dipole_map = 1+np.dot(stacked_vectors,dipole)
+        dipole_map = 1 + np.dot(stacked_vectors, dipole)
         return dipole_map
     
     def modulated_map(self,
-            l_max_input: int | None = None,
-            scaling_factor: float = 100,
+            scaling_factor: float = 100.,
     ) -> NDArray[np.int_]:
         '''
         Generate a dipole modulated sky from an SKA map.
         
-        :param l_max_input: The highest multiple to generate during the spherical 
-            harmonic decomposition of the map. The default is 3 * nside - 1.
         :param scaling factor: The factor by which alm's have to be scaled before
             modulating the map. Used to ensure we have large enough numbers that 
-            we don't lose the dipole in the in the poisson draw The default is 100.
+            we don't lose the dipole in the in the poisson draw.
         
         :return: Dipole modulated Healpy density map.
         '''
-        nside = hp.npix2nside(len(self.density_map))
-        if l_max_input is None:
-            l_max = 3 * nside - 1
-        else:
-            l_max = l_max_input
-
-        # TODO: understand what is happening below with respect to
-        # negatives in the power map and needing to downgrade the
-        # dipole-modulated map
-        self.alm = hp.map2alm(self.density_map)
-        self.scaled_alm = scaling_factor * self.alm
-        self.reconstructed_map = hp.alm2map(self.scaled_alm, nside=nside)
-        self.dipole_modulation = self.dipole_map(nside)
-        modulated_map = self.reconstructed_map * self.dipole_modulation
-        modulated_map_dscaled = hp.ud_grade(
-            modulated_map,
-            nside_out=64,
-            power=-2
-        )   
-        self.lambdas = modulated_map_dscaled / scaling_factor
-        final_map = np.random.poisson(
-            modulated_map_dscaled / scaling_factor
+        # NOTE: The SKA power spectrum is truncated beyond ell = 512, which is
+        # also the nside of the map. Therefore, one needs to truncate the power
+        # spectrum when calling map2alm by specifying lmax, otherwise there may
+        # be cells with negative rate paramaters due to higher order harmonics
+        # beyond ell = 512.
+        self.scaling_factor = scaling_factor
+        self.nside = hp.npix2nside(len(self.density_map))
+        self.lmax = self.nside
+        assert self.nside == 512, f'''Map of nside {self.nside} used.
+Please use the native nside=512 maps instead.'''
+        
+        self.alm = hp.map2alm(self.density_map, lmax=self.lmax)
+        self.scaled_alm = self.scaling_factor * self.alm
+        self.reconstructed_map = hp.alm2map(
+            self.scaled_alm,
+            nside=self.nside,
+            lmax=self.lmax
         )
+        
+        self.dipole_modulation = self.dipole_map(self.nside)
+        self.dipole_added_map = self.reconstructed_map * self.dipole_modulation
+        self.lambdas = self.dipole_added_map / self.scaling_factor
+        final_map = np.random.poisson(self.lambdas)
+        
         return final_map
