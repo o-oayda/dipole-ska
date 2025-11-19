731204c7147130082cdc47ca2aed41a5cb71d999
Oliver Oayda <oaydaoliver@gmail.com>
2025-11-19 20:58:11 +1100

    wip: rough working script to iterate multipole fits

--- dipoleska/results/ska_fit_multipoles.py
+++ dipoleska/results/ska_fit_multipoles.py
@@ -7,21 +7,20 @@ from dipoleska.utils.plotting import MapPlotter
 from dipoleska.models.multipole import Multipole
 import matplotlib.pyplot as plt
 import argparse
-import numpy as np
-import healpy as hp
 
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Fit multipoles to SKA maps."
     )
-    parser.add_argument(
-        '--newsizes',
-        action='store_true'
-    )
+    # parser.add_argument(
+    #     '--newsizes',
+    #     action='store_true'
+    # )
     parser.add_argument(
         '--likelihood',
-        choices=['general_poisson', 'general_poisson_rms']
+        choices=['general_poisson', 'general_poisson_rms'],
+        default='general_poisson'
     )
     parser.add_argument(
         '--no-plots',
@@ -46,11 +45,13 @@ def build_run_name(args: argparse.Namespace) -> str:
 
 
 def main() -> None:
+    OUTPUT_DIR = 'dipoleska/results/runs'
+
     parser = build_parser()
     args = parser.parse_args()
     plot_enabled = not args.no_plots
 
-    loader = MapCollectionLoader()
+    loader = MapCollectionLoader(use_base_rms=True)
     loader.load(filter_attrs={'newsizes': True})
     data = loader.map_collections
     data = cast(list[dict[str, Any]], data)
@@ -60,33 +61,38 @@ def main() -> None:
         rmsmap = collection['files']['rms']['data']
         COLLECTION_ID = collection['id']
         LIKELIHOOD = args.likelihood
-        PATH_TO_DMAP = collection['files']['counts']['path']
-        subdirs = PATH_TO_DMAP.split('/')
-        relative_path_in_data = '/'.join(map(str, subdirs[2:4])) # remove data/ska
+        run_name = f'{COLLECTION_ID}_{LIKELIHOOD}_ells{'-'.join(map(str, args.ells))}'
+        RUN_DIR = os.path.join(OUTPUT_DIR, run_name)
+        os.makedirs(RUN_DIR, exist_ok=True)
 
         processor = MapProcessor([dmap, rmsmap])
-        processor.mask(output_frame='C', load_from_file='gal10_ps')
+        if 'gal10.0' in COLLECTION_ID:
+            mask = 'gal10_ps'
+        elif 'gal5.0' in COLLECTION_ID:
+            mask = 'gal5_ps'
+        else:
+            raise Exception('Not sure which mask to use yet.')
+        processor.mask(output_frame='C', load_from_file=mask)
         masked_dmap, masked_rmsmap = processor.density_maps
-
-        hp.projview(rmsmap)
-        plt.show()
 
         if plot_enabled:
             plotter = MapPlotter(masked_dmap)
-            plotter.plot_density_map(
+            _, fig = plotter.plot_density_map(
                 projview_kwargs={'badcolor': 'grey', 'coord': ['C', 'G']}
             )
-            plotter.plot_smooth_map(
+            fig.savefig(os.path.join(RUN_DIR, 'dmap.pdf'), bbox_inches='tight')
+            _, fig = plotter.plot_smooth_map(
                 projview_kwargs={'badcolor': 'grey', 'coord': ['C', 'G']}
             )
-            plt.show()
+            fig.savefig(os.path.join(RUN_DIR, 'smoothed_dmap.pdf'), bbox_inches='tight')
+
+            plt.close()
 
         if 1 in args.ells:
             prior = Prior(choose_prior={'M1': ['Uniform', 0., 0.05]})
         else:
             prior = None
 
-        print(collection)
         model = Multipole(
             masked_dmap,
             ells=args.ells,
@@ -94,26 +100,29 @@ def main() -> None:
             likelihood=LIKELIHOOD,
             rms_map=masked_rmsmap
         )
-        step = True
+        step = False
+        for ell in args.ells:
+            if ell > 1:
+                step = True
+                break
 
-        if plot_enabled:
-            model.prior.plot_priors()
-
-        run_name = f'{COLLECTION_ID}_{LIKELIHOOD}_ells{'-'.join(map(str, args.ells))}'
         model.run_nested_sampling(
-            step=step, run_name=run_name, output_dir=os.path.join('dipoleska/results/runs', relative_path_in_data)
+            step=step,
+            run_name=run_name, 
+            output_dir=OUTPUT_DIR
         )
-        RUN_DIR = model.get_run_dir()
 
         if plot_enabled:
             model.corner_plot(
                 backend='getdist', coordinates=['equatorial', 'galactic'],
-                save_path=RUN_DIR
+                save_path=os.path.join(RUN_DIR, 'corner.pdf')
             )
             model.sky_direction_posterior(
-                coordinates=['equatorial', 'galactic'], save_path=RUN_DIR
+                coordinates=['equatorial', 'galactic'],
+                save_path=os.path.join(RUN_DIR, 'sky_proj.pdf'),
+                contour_levels=[1, 2]
             )
-            plt.show()
+            plt.close()
 
 
 if __name__ == '__main__':
