3899d371ac9e0c7fdd6a62c731bb85e90a0eb530
Oliver Oayda <oaydaoliver@gmail.com>
2025-11-24 11:40:34 +1100

    fix: update posterior power spec example script

--- dipoleska/examples/posterior_power_spectrum.py
+++ dipoleska/examples/posterior_power_spectrum.py
@@ -1,15 +1,17 @@
 #%%
 from dipoleska.utils.modulated_map_generator import ModulatedMapGenerator
-from dipoleska.powerspectrum.power_spectrum_plotter import PowerSpectrumPlotter
+from dipoleska.powerspectrum.power_spectrum_plotter import LegacyPowerSpectrumPlotter
 from dipoleska.utils.map_process import MapProcessor
 from dipoleska.utils.map_read import MapLoader
 from dipoleska.models.multipole import Multipole
 from dipoleska.powerspectrum.posterior_power_spectrum import PosteriorPowerSpectrum
-import healpy as hp
-import numpy as np
+import matplotlib.pyplot as plt
 #%%
+
+LIKELIHOOD = 'general_poisson'
+
 # Load a SKA map, modulate it with a dipole, process it, and run multipole model.
-loader = MapLoader(-1, 'AA')
+loader = MapLoader(1, 'AA')
 density_map = loader.load(101)
 modulator = ModulatedMapGenerator(
         density_map,
@@ -21,21 +23,25 @@ dipole_map = modulator.modulated_map(scaling_factor=100)
 processor = MapProcessor(dipole_map)
 processor.change_map_resolution(nside_out=64)
 density_map_dscaled = processor.density_map
-model = Multipole(density_map_dscaled, ells=[1, 2])
+model = Multipole(density_map_dscaled, likelihood=LIKELIHOOD, ells=[0, 1, 2])
 model.run_nested_sampling(step=True)
 #%%
 # Calculate the posterior power spectrum from the model samples.
 # Note that we need power spectrum for same nside as the SKA maps.
-model_for_power_spectrum = Multipole(density_map, ells=[1, 2])
-posteriorps = PosteriorPowerSpectrum(model._samples,
-                                     model_for_power_spectrum.model,
-                                     sample_count=500)
+posteriorps = PosteriorPowerSpectrum(
+    sample_chains=model.samples,
+    model=model.model,
+    likelihood=LIKELIHOOD,
+    sample_count=500
+)
 cl_mean, cl_std = posteriorps.power_spectrum_calculator()
 #%%
 # Plot the power spectra including the posterior power spectrum.
-ca, cf = PowerSpectrumPlotter(101, 150, -1, 'AA').plot_power_spectra()
+ca, cf = LegacyPowerSpectrumPlotter(101, 150, 1, 'AA').plot_power_spectra()
 ca.errorbar(1, cl_mean[1], yerr=cl_std[1], fmt='o', color='red', label='Sampled $C_l$ at $l=1$',elinewidth=2)
 ca.errorbar(2, cl_mean[2], yerr=cl_std[2], fmt='o', color='blue', label='Sampled $C_l$ at $l=2$',elinewidth=2)
 ca.set_ylim(1e-8, 1e-2)
 ca.set_xlim(0.9,len(cl_mean))
 ca.legend()
+
+plt.show()
